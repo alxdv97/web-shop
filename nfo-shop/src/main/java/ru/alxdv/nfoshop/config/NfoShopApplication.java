04cdb6933ab97016a1704d8c73e4effbc5b13940
Деев Алексей <adeev@neoflex.ru>
2021-02-24 10:59:14 +0300

    Initial commit

---
+++ nfo-shop/src/main/java/ru/alxdv/nfoshop/config/NfoShopApplication.java
@@ -0,0 +1,15 @@
+package ru.alxdv.nfoshop.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfoShopApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NfoShopApplication.class, args);
+    }
+
+}
---
+++ nfo-shop/test/ru/alxdv/nfoshop/NfoShopApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.alxdv.nfoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfoShopApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
